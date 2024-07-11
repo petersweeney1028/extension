445b5af551f176f97b7511f6f1ddc86749a77f08
Peter <petersweeney1028@gmail.com>
2024-07-10 21:56:15 -0400

    fixing network connection issue

--- chrome-extension-backend/app.js
+++ chrome-extension-backend/app.js
@@ -10,9 +10,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-console.log('MongoDB URI:', MONGO_URI); // This should print the correct URI
+console.log('MongoDB URI:', MONGO_URI);
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
